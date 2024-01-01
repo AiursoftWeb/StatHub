8bc867167474497c68173aa1590a57f43ff79446
AnduinXue <anduin@aiursoft.com>
2024-01-01 13:56:36 +0000

    Simpify install script.

--- src/Aiursoft.StatHub.Server/Controllers/HomeController.cs
+++ src/Aiursoft.StatHub.Server/Controllers/HomeController.cs
@@ -22,4 +22,37 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    [HttpGet("install.sh")]
+    public IActionResult GetInstallScript()
+    {
+        // return text/plain
+        var installScript = @$"sudo apt install dotnet7
+sudo dotnet tool install Aiursoft.StatHub.Client --tool-path /opt/stathub-client || sudo dotnet tool update Aiursoft.StatHub.Client --tool-path /opt/stathub-client
+echo ""[Unit]
+Description=Stathub Client
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/opt/stathub-client/stathub-client -s ""{Request.Scheme}://{Request.Host}""
+WorkingDirectory=/opt/stathub-client
+Restart=always
+RestartSec=10
+KillSignal=SIGINT
+Environment=\""ASPNETCORE_ENVIRONMENT=Production\""
+Environment=\""DOTNET_PRINT_TELEMETRY_MESSAGE=false\""
+Environment=\""DOTNET_CLI_TELEMETRY_OPTOUT=1\""
+Environment=\""ASPNETCORE_FORWARDEDHEADERS_ENABLED=true\""
+
+[Install]
+WantedBy=multi-user.target"" | sudo tee /etc/systemd/system/stathub.service
+sudo systemctl daemon-reload
+sudo systemctl enable stathub.service
+sudo systemctl stop stathub.service > /dev/null 2>&1
+sudo systemctl start stathub.service";
+        return Content(installScript, "text/plain");
+    }
 }
