549b08fed059120aee29042f93a343063c831b1d
AnduinXue <anduin@aiursoft.com>
2024-01-01 13:36:17 +0000

    Show clients as table.

--- src/Aiursoft.StatHub.Server/Controllers/HomeController.cs
+++ src/Aiursoft.StatHub.Server/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aiursoft.StatHub.Server.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aiursoft.StatHub.Server.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly InMemoryDatabase _database;
+
+    public HomeController(InMemoryDatabase database)
+    {
+        _database = database;
+    }
+    
     public IActionResult Index()
     {
-        return View();
+        var clients = _database.GetClients();
+        return View(clients);
     }
 }
