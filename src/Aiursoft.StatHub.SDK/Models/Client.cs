0755b710d4ea3e3a5da4e81c9c8d82d09978bc7a
AnduinXue <anduin@aiursoft.com>
2024-01-04 12:21:51 +0000

    Fix CPU usage.

--- src/Aiursoft.StatHub.SDK/Models/Client.cs
+++ src/Aiursoft.StatHub.SDK/Models/Client.cs
@@ -4,15 +4,22 @@ namespace Aiursoft.StatHub.SDK.Models;
 
 public class Client
 {
+    private MessageStage<int> _cpuUsage;
+    public Client()
+    {
+        Stats = new AsyncObservable<DstatResult>();
+        _cpuUsage = Stats.Map(stat => 100 - stat.CpuIdl).StageLast();
+    }
+    
     public string Hostname { get; set; } = null!;
     
     public string Ip { get; set; } = null!;
 
     public DateTime BootTime { get; set; } = DateTime.MinValue;
 
-    public MessageStage<int> GetCpuUsage()
+    public int GetCpuUsage()
     {
-        return Stats.Map(stat => 100 - stat.CpuIdl).StageLast();
+        return _cpuUsage.Stage;
     }
 
     public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
@@ -20,5 +27,5 @@ public class Client
     public string Version { get; set; } = null!;
     public string Process { get; set; } = null!;
     
-    public AsyncObservable<DstatResult> Stats { get; set; } = new();
+    public AsyncObservable<DstatResult> Stats { get; set; }
 }
