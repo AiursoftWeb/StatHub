df3d0d22b2628114faa68a053617d8360dad7655
AnduinXue <anduin@aiursoft.com>
2024-01-04 16:10:46 +0000

    Fix an issue that client not working at all.

--- src/Aiursoft.StatHub.Client/Services/ServerMonitor.cs
+++ src/Aiursoft.StatHub.Client/Services/ServerMonitor.cs
@@ -3,7 +3,6 @@ using Aiursoft.AiurObserver;
 using Aiursoft.AiurObserver.Command;
 using Aiursoft.StatHub.SDK.Models;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Aiursoft.StatHub.Client.Services;
 
@@ -25,21 +24,23 @@ public class ServerMonitor
         _logger = logger;
     }
 
-    public Task MonitorServerAsync(CancellationToken cancellationToken, bool onlyOneTrigger = false)
+    public async Task MonitorServerAsync(CancellationToken cancellationToken, bool onlyOneTrigger = false)
     {
+        try
+        {
             _logger.LogInformation("Starting to monitor server...");
             _subscription = _commandService
                 .Output
-                .Pipe(t => { _logger.LogTrace("Received metrics: {Trace}.", t); })
                 .Filter(t => !string.IsNullOrWhiteSpace(t))
                 .Filter(t => !t.StartsWith("----"))
                 .Filter(t => !t.StartsWith("usr"))
                 .Map(t => t.Replace("|", " "))
                 .Filter(t => t.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length >= 16)
+                .Pipe(t => { _logger.LogTrace("Received dstat metrics: {Trace}.", t); })
                 .Map(t => new DstatResult(t))
                 .Aggregate(10) // Aggregate 10 results into one array. 
                 .Throttle(TimeSpan.FromSeconds(9)) // Max speed is every 9 seconds one request.
-                .Pipe(result => { _logger.LogTrace("Sending metrics: {Trace}.", JsonConvert.SerializeObject(result)); })
+                .Pipe(result => { _logger.LogTrace("Sending {Count} metrics.", result.Length); })
                 .Subscribe(_submitService);
 
             var args = "--cpu --mem --disk --net --load --nocolor --noheaders";
@@ -48,7 +49,12 @@ public class ServerMonitor
                 args += " 1 1";
             }
 
-            return _commandService.Run("dstat", args, Directory.GetCurrentDirectory());
+            await _commandService.Run("dstat", args, Directory.GetCurrentDirectory());
+        }
+        finally
+        {
+            _subscription?.Unsubscribe();
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
