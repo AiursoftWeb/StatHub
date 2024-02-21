dadc00216687df86bd2b588f208aa210934f4c26
xuef <xuef@microsoft.com>
2024-02-21 09:31:51 +0000

    Add WebSocket stats to client detail page
    A new section displaying WebSocket Lag statistics has been added to the client details page in StatHub. This includes a chart visualizing WebSocket connection data and a log filter to tune the displayed data. Additionally, a new JavaScript file, metrics.js, has been created to manage WebSocket connections and chart updates. The Chart.js library, previously commented out, has been turned back on to support this feature.

---
+++ src/Aiursoft.StatHub.Server/wwwroot/js/metrics.js
@@ -0,0 +1,73 @@
+'use strict';
+let webSocket;
+const wsChartCtx = document.getElementById('wsChart').getContext('2d');
+const wsChartData = {
+    labels: [],
+    datasets: [{
+        label: "WebSocket Connection",
+        backgroundColor: 'rgb(2, 232, 99)',
+        borderColor: 'rgb(2, 232, 99)',
+        fill: false,
+        data: []
+    }]
+};
+
+const chartOption = {
+    responsive: true,
+    tooltips: {
+        mode: 'index',
+        intersect: false
+    },
+    hover: {
+        mode: 'nearest',
+        intersect: true
+    },
+    scales: {
+        xAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'Time'
+            }
+        }],
+        yAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'Value'
+            }
+        }]
+    }
+};
+
+window.myWsLine = new Chart(wsChartCtx, {
+    type: 'line',
+    data: wsChartData,
+    options: chartOption
+});
+
+const getWSAddress = function () {
+    const isHttps = 'https:' === document.location.protocol;
+    const host = window.location.host;
+    const head = isHttps ? "wss://" : "ws://";
+    return head + host;
+};
+
+const updateWebSocketChart = function (evt) {
+    if (wsChartData.labels.length > 30) {
+        wsChartData.labels.shift();
+        wsChartData.datasets[0].data.shift();
+    }
+    wsChartData.labels.push(new Date().toLocaleTimeString());
+    wsChartData.datasets[0].data.push(evt);
+    window.myWsLine.update();
+};
+
+const startWebSocketClient = function (machineId) {
+    webSocket = new WebSocket(getWSAddress() + "/metrics/" + machineId + "/cpu.ws");
+    webSocket.onmessage = function (evt) {
+        setTimeout(function () {
+            updateWebSocketChart(evt);
+        }, 0);
+    };
+};
