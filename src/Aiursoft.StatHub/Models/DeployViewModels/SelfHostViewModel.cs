7ddb77295b8b42138c1e6981ee2c4e6cab96afbc
Anduin Xue <anduin@aiursoft.com>
2026-01-09 12:32:16 +0000

    Add new self host page.

---
+++ src/Aiursoft.StatHub/Models/DeployViewModels/SelfHostViewModel.cs
@@ -0,0 +1,11 @@
+using Aiursoft.UiStack.Layout;
+
+namespace Aiursoft.StatHub.Models.DeployViewModels;
+
+public class SelfHostViewModel : UiStackLayoutViewModel
+{
+    public SelfHostViewModel()
+    {
+        PageTitle = "Self Host";
+    }
+}
