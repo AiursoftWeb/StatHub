8c9d173ea6ff311038c12324ddbd7712e24b4170
Anduin Xue <anduin@aiursoft.com>
2026-02-11 12:04:57 +0000

    Bump template.

--- src/Aiursoft.StatHub/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
+++ src/Aiursoft.StatHub/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
@@ -2,5 +2,6 @@ namespace Aiursoft.StatHub.Views.Shared.Components.MarketingNavbar;
 
 public class MarketingNavbarViewModel
 {
-
+    public string ProjectName { get; set; } = "Aiursoft StatHub";
+    public string LogoUrl { get; set; } = "/logo.svg";
 }
